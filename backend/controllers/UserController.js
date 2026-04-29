79524d5fb6cecfa56055f569adbd40d142021628
analivalmeida <analivialpalmeida@gmail.com>
2026-04-29 12:44:56 -0300

    Aula2904 - Liskowski em validation-token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -128,4 +128,10 @@ module.exports = class UserController {
         }
         res.status(200).json(user)
     }
+
+    static async editUser(req, res) {
+        res.status(200).json({
+            message: 'Usuário atualizado com sucesso!'
+        })
+    }
 }
